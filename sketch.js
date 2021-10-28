03d748d0c109b935d29bd3b1120c1ff4f8eb6e32
Anton Popov <apwork03@gmail.com>
2021-10-29 01:44:22 +0300

    small fixes

--- sketch.js
+++ sketch.js
@@ -3,9 +3,9 @@ let offset = 0;
 let button;
 
 function setup(){
-  canvas = createCanvas(800, 800, WEBGL);
+  canvas = createCanvas(600, 600, WEBGL);
   canvas.position(windowWidth/2-canvas.width/2, windowHeight/2-canvas.height/2);
-  background(240);
+  background(245);
   stroke(0);
   strokeWeight(10);
   n = 5;
@@ -22,11 +22,11 @@ function setup(){
 }
 
 function draw(){
-  for (let i=0; i<10; i++){
-  let r6 = 150 * (sin(radians(offset*r1))+sin(radians(offset*r2)));
-  let r7 = 150 * (sin(radians(offset*r3))+cos(radians(offset*r4)));
+  for (let i=0; i<20; i++){
+  let r6 = 120 * (sin(radians(offset*r1))+sin(radians(offset*r2)));
+  let r7 = 120 * (sin(radians(offset*r3))+cos(radians(offset*r4)));
   point(r6, r7);
-  offset += 0.2;
+  offset += 0.1;
   }
 }
 
@@ -35,7 +35,7 @@ function reset(){
     r2 = round(random(1,n));
     r3 = round(random(1,n));
     r4 = round(random(1,n));
-    background(240);
+    background(245);
 
 }
 
