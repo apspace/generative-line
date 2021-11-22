20496ed3db29c9d3e4627e5a07d9870fc4ab77ce
Anton Popov <apwork03@gmail.com>
2021-11-22 17:28:53 +0300

    Updated GUI

--- sketch.js
+++ sketch.js
@@ -2,12 +2,7 @@ let r1, r2, r3, r4, n;
 let offset = 0;
 let bg;
 let button, controls, refresh;
-let slider, dSlider, pSlider, dButton;
-let font;
-
-function preload(){
-  font = loadFont('font/GorkaSans-Regular.otf')
-}
+let slider, dSlider, pSlider, dButton, info;
 
 function setup(){
   bg = color('#f1f1f1');
@@ -26,20 +21,18 @@ function setup(){
   dButton.addClass('button');
   button = createButton('Save Image');
   button.parent(dButton);
-  // button.parent(controls);
   refresh = createButton('Refresh');
   refresh.addClass('button');
   refresh.parent(dButton);
 
+  info = createDiv();
+  info.addClass('info');
 
   canvas = createCanvas(600, 600, WEBGL);
-  // canvas.parent(controls);
-  // canvas.style('z-index', '-1');
   background(bg);
 
   button.mousePressed(save);
   refresh.mousePressed(reset);
-  // canvas.mousePressed(reset);
 
 
   n = 3;
@@ -47,17 +40,16 @@ function setup(){
   r2 = round(random(1,n));
   r3 = round(random(1,n));
   r4 = round(random(1,n));
-   textFont(font, 14);
 }
 
 function draw(){
   background(bg);
   n = slider.value();
-  // orbitControl(8,8);
-  push();
-  rotateY(radians(frameCount));
-  rotateX(radians(-30));
-  rotateZ(PI);
+  orbitControl(8,8);
+  // push();
+  // rotateY(radians(frameCount));
+  // rotateX(radians(-30));
+  // rotateZ(PI);
   strokeWeight(2);
   stroke(255,0, 0);
   line(0,0,0, 100, 0, 0);
@@ -74,11 +66,10 @@ function draw(){
     point(r6, r7, r8);
     // offset += 0.1;
   }
-  pop();
   fill(0);
   let string = 'R1: '+r1+' R2:'+r2+' R3:'+r3+' R4:'+r4;
-  text(string, -canvas.width/2+10 ,-canvas.height/2+20);
   pSlider.html('Complexity: '+slider.value());
+  info.html(string);
 
 }
 
